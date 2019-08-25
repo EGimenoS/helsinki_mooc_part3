d171b514b267754cd951e9d6bd8529398b44a1cf
EgimenoS <ergisan@gmail.com>
2019-08-25 17:57:57 +0200

    3.12 formatted

--- mongo.js
+++ mongo.js
@@ -17,7 +17,7 @@ const Person = mongoose.model("Person", personSchema);
 const getAll = () => {
   Person.find({}).then(result => {
     result.forEach(person => {
-      console.log(person);
+      console.log(person.name, person.number);
     });
     mongoose.connection.close();
   });
